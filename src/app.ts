b983052c9e09a34fc3a35e563b374817032d02fb
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-11-10 08:58:02 -0300

    Using function handlers for routes

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,20 @@ export const app:Application = express();
 app.use(express.json()); // parses incoming requests with JSON payloads
 app.use(express.urlencoded({ extended: true })) // parses incoming requests with urlencoded payloads
 
-app.get('/', (req, res) => {
-    res.end('Hello, challenge.')
-})
+
+app.get('/', statusHandler);
+
+app.get('/products', listProductsHandler);
+
+// All the following routes will have the format:
+// /products/<any-number>
+// e.g: /products/123
+const productIdRoute = '/products/:code([0-9]+)'
+
+app.get(productIdRoute, specificProductHandler)
+
+app.put(productIdRoute, updateProductHandler)
+
+app.delete(productIdRoute, deleteHandler)
+
+
