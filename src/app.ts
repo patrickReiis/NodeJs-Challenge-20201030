d1619463f6d12d54358c67c2debfc7d2904a5384
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-11-10 07:50:57 -0300

    Routes sample

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, {Application, Request, Response } from 'express';
+
+export const app:Application = express();
+
+// middleware
+app.use(express.json()); // parses incoming requests with JSON payloads
+app.use(express.urlencoded({ extended: true })) // parses incoming requests with urlencoded payloads
+
+app.get('/', (req, res) => {
+    res.end('Hello, challenge.')
+})
