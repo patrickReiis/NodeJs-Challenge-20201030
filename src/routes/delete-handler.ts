b5425d6b77ef9c2869589eed0ca971bc56d26cf4
PyyHermit <76563803+PyyHermit@users.noreply.github.com>
2022-11-14 14:12:07 -0300

    Delete endpoint done

--- src/routes/delete-handler.ts
+++ src/routes/delete-handler.ts
@@ -1,5 +1,23 @@
 import {Request, Response } from 'express';
+import { dataSource } from '../db/get-data-source';
+import { Product } from '../db/entity/Product';
+import { doesUserExist } from '../utils';
 
-export function deleteHandler(req:Request, res:Response) {
-    res.end('change status to trash');
+export async function deleteHandler(req:Request, res:Response) {
+
+    if (await doesUserExist(req.url.slice('/products/'.length)) == true) {
+        const productCode = decodeURIComponent(req.url.slice('/products/'.length));
+        const product = await dataSource
+            .query(`select * from product where food -> 'code' ? '${productCode}'`);
+
+        product[0].status = 'trash';
+
+        await dataSource.getRepository(Product).save(product);
+        res.writeHead(200);
+        res.end()
+        return
+    }
+
+    res.writeHead(404);
+    res.end();
 }
